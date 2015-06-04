394b111177a51843d8d733322374ceb820422a59
Karl Hesselgren <karl.hesselgren@gmail.com>
2015-06-04 21:15:09 +0200

    Updated assets

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,19 +1,63 @@
-// Ionic Starter App
+var App = angular.module('Inbreed', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+App.service('LoadInbreed', ['$http', LoadInbreed]);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+function LoadInbreed($http) {
+    this.getContent = function ($scope) {
+        $http.get('test.json').success(function (result) {
+          $scope.tabs     = result.tabs;
+          $scope.schema   = result.schema;
+          $scope.header	  = result.header;
+        }).error(function (result) {
+        });
+    };
+}
+
+App.controller('InbreedCtrl', function ($scope, $ionicSideMenuDelegate, $ionicModal, $ionicSlideBoxDelegate, LoadInbreed) {
+
+    $scope.setSelectedBand = function (index) {
+        $scope.selectedBand = index;
+    };
+
+    $scope.currentSlide = 0;
+
+    $scope.isActive = function (item) {
+        return $scope.currentSlide == item;
+    };
+
+    $scope.refresh  = LoadInbreed.getContent($scope);
+
+    $scope.setActiveSlide = function (index) {
+        $ionicSlideBoxDelegate.slide(index);
+        $scope.currentSlide = index;
+    };
+
+    $ionicModal.fromTemplateUrl('band_closeup.html', function (modal) {
+        $scope.taskModal = modal;
+    }, {
+        scope: $scope
+    });
+
+    $scope.openModal = function () {
+        $scope.taskModal.show();
+    };
+
+    $scope.closeModal = function () {
+        $scope.taskModal.hide();
+    };
+
+    $ionicModal.fromTemplateUrl('map.html', function (modal) {
+        $scope.mapModal = modal;
+    }, {
+        scope: $scope
+    });
+
+    $scope.openMapModal = function () {
+        $scope.mapModal.show();
+    };
+
+    $scope.closeMapModal = function () {
+        $scope.mapModal.hide();
+    };
+
+});
